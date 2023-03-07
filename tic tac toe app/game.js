06c3fd6db1691e01274e987164e405bb69d32ecb
Bharath <kadimcherlabharat123@gmail.com>
2023-03-07 18:57:21 +0530

    Added option to enter name of players

--- tic tac toe app/game.js
+++ tic tac toe app/game.js
@@ -3,23 +3,24 @@ $(document).ready(function() {
   var squares = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   var wins = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
   var gameOver = false;
+  var player1Name = '';
+  var player2Name = '';
   
   $('.square').click(function() {
     if (!gameOver && $(this).text() === '') {
-      if (turn % 2 === 1) {
-        $(this).text('X');
-        squares[$(this).attr('id')-1] = 'X';
-      } else {
-        $(this).text('O');
-        squares[$(this).attr('id')-1] = 'O';
-      }
+      var playerName = turn % 2 === 1 ? player1Name : player2Name;
+      var playerMark = turn % 2 === 1 ? 'X' : 'O';
+      $(this).text(playerMark);
+      squares[$(this).attr('id')-1] = playerMark;
       $(this).addClass('clicked');
       turn++;
-      checkForWinner();
+      checkForWinner(playerName);
     }
   });
   
   $('#reset').click(function() {
+    player1Name = $('#player1').val();
+    player2Name = $('#player2').val();
     turn = 1;
     squares = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     gameOver = false;
